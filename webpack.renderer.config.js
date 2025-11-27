889b6f74305654a25a4d45d8a0b4b36476df516b
Jean Carlos Lopes Lellis <jeanlopes2166@gmail.com>
2025-11-26 21:00:17 -0300

    Refactor webpack renderer config for clarity

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -1,11 +1,9 @@
 const rules = require('./webpack.rules');
 const webpack = require('webpack');
 
-// Filtra regras CSS antigas
 const filteredRules = rules.filter(rule => rule.test && rule.test.toString() !== /\.css$/.toString());
 
 module.exports = {
-  // Importante: Deixe sem 'target' ou use 'electron-renderer' padrão
   
   module: {
     rules: [
@@ -18,20 +16,17 @@ module.exports = {
   },
   resolve: {
     extensions: ['.js', '.jsx', '.json', '.css'],
-    // Se alguma lib pedir path ou fs, entregamos vazio para não quebrar no front
     fallback: {
       "path": false,
       "fs": false
     }
   },
   plugins: [
-    // Força a substituição no código compilado
     new webpack.DefinePlugin({
       '__dirname': JSON.stringify('/'),
       'global': 'window',
     }),
   ],
-  // Configuração nativa do Webpack 5 para simular Node
   node: {
     __dirname: true,
     __filename: true,
